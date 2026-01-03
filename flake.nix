c2def185dfefda573e0b7ea04a9185e5c4fe0d28
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-01-03 12:40:03 -0600

    flake-parts

--- flake.nix
+++ flake.nix
@@ -1,50 +1,129 @@
 {
-  inputs.clan-core.url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
-  inputs.nixpkgs.follows = "clan-core/nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    clan-core = {
+      url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        flake-parts.follows = "flake-parts";
+        disko.follows = "disko";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    dotfiles = {
+      url = "github:UnstoppableMango/dotfiles";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        flake-parts.follows = "flake-parts";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    nixos = {
+      url = "github:UnstoppableMango/nixos";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        disko.follows = "disko";
+        dotfiles.follows = "dotfiles";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    {
-      self,
-      clan-core,
-      nixpkgs,
+    inputs@{
+      flake-parts,
       ...
-    }@inputs:
-    let
-      # Usage see: https://docs.clan.lol
-      clan = clan-core.lib.clan {
-        inherit self;
-        imports = [ ./clan.nix ];
-        specialArgs = { inherit inputs; };
+    }:
+    # let
+    #   # Usage see: https://docs.clan.lol
+    #   clan = clan-core.lib.clan {
+    #     inherit self;
+    #     imports = [ ./clan.nix ];
+    #     specialArgs = { inherit inputs; };
 
-        # Customize nixpkgs
-        # pkgsForSystem =
-        #   system:
-        #   import nixpkgs {
-        #     inherit system;
-        #     config = {
-        #       allowUnfree = true;
-        #     };
-        #     overlays = [];
-        #   };
-      };
-    in
-    {
-      inherit (clan.config) nixosConfigurations nixosModules clanInternals;
-      clan = clan.config;
-      # Add the Clan cli tool to the dev shell.
-      # Use "nix develop" to enter the dev shell.
-      devShells =
-        nixpkgs.lib.genAttrs
-          [
-            "x86_64-linux"
-            "aarch64-linux"
-            "aarch64-darwin"
-            "x86_64-darwin"
-          ]
-          (system: {
-            default = clan-core.inputs.nixpkgs.legacyPackages.${system}.mkShell {
-              packages = [ clan-core.packages.${system}.clan-cli ];
+    #     # Customize nixpkgs
+    #     # pkgsForSystem =
+    #     #   system:
+    #     #   import nixpkgs {
+    #     #     inherit system;
+    #     #     config = {
+    #     #       allowUnfree = true;
+    #     #     };
+    #     #     overlays = [];
+    #     #   };
+    #   };
+    # in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = with inputs; [
+        clan-core.flakeModules.default
+        treefmt-nix.flakeModule
+      ];
+
+      clan = {
+        imports = [ ./clan.nix ];
+
+        machines = {
+          agreus = {
+            imports = with inputs; [
+              nixos.nixosModules.agreus
+            ];
+
+            nixpkgs.hostPlatform = "x86_64-linux";
+
+            # Enable remote Clan commands over SSH
+            clan.core.networking.targetHost = "root@jon";
+
+            # Disk configuration
+            disko.devices.disk.main = {
+              device = "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b448b4aec2929";
             };
-          });
+          };
+        };
+      };
+
+      # inherit (clan.config) nixosConfigurations nixosModules clanInternals;
+      # clan = clan.config;
+
+      perSystem =
+        { inputs', pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              inputs'.clan-core.packages.clan-cli
+              nixfmt
+            ];
+          };
+
+          treefmt = {
+            programs.nixfmt.enable = true;
+          };
+        };
     };
 }
